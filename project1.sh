95083eb39284c0d3e55078fa1aaa49a5aefa50da
Feng <54845390+Acha0623@users.noreply.github.com>
2020-03-17 20:04:04 +0100

    Update project1.sh

--- project1.sh
+++ project1.sh
@@ -1,22 +1,29 @@
 #date=0317
 awk '{print $1,$2}' GorgorWholeGen.ped   #check the subspecies information of griollas
 
-nano sampleID.txt # picked 4 individuals of each subspecies
+# picked 8 individuals of each subspecies
+nano sampleID.txt 
 
+Gbb-Maisha Gbb-Maisha
 Gbb-N010_Turimaso Gbb-N010_Turimaso
-Gbg-9732_Mkubwa Gbg-9732_Mkubwa
+Gbb-Umurimo Gbb-Umurimo
+Gbg-A929_Kaisi Gbg-A929_Kaisi
+Gbg-N011_Pinga Gbg-N011_Pinga
+Gbg-N012_Dunia Gbg-N012_Dunia
 Ggd-B646_Nyango Ggd-B646_Nyango
-Ggg-B647_Anthal Ggg-B647_Anthal
+Ggg-A932_Mimi Ggg-A932_Mimi
+Ggg-KB5792_Carolyn Ggg-KB5792_Carolyn
+Ggg-X00108_Abe Ggg-X00108_Abe
 
 
 #make bed file for whole genome file of all individuals
 plink --file GorgorWholeGen --make-bed --out GorgorWholeGen 
 #pick the whole genomen of 4 individuals we want
-plink --bfile GorgorWholeGen --noweb --keep sampleID.txt --recode --out ../datatest21/testdata
+plink --bfile GorgorWholeGen --noweb --keep sampleID.txt --recode --out ../datatest21/test10
 #make bed file for whole genome file of 4 individuals we want 
-plink --file testdata --make-bed --out testdata
+plink --file test10 --make-bed --out test10
 #pick chromosome 21 of 4 individuals 
-plink --bfile testdata --chr 21 --out test21
+plink --bfile test10 --chr 21 --out 待定
 
 #########END###########
-123
+
