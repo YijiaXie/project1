3a140945dc2119ed34264ddb5560ddb0a6e4b596
Feng <54845390+Acha0623@users.noreply.github.com>
2020-03-19 23:52:23 +0100

    Update do_CVerrorplot.r

--- do_CVerrorplot.r
+++ do_CVerrorplot.r
@@ -1,3 +1,4 @@
 data <- read.table('CVerror.txt', sep = '', header = T)
 png('CVerror.png', width = 1000, height = 800)
 plot(data[,1], data[,2], type = 'o', pch = 19, xlab = 'k', ylab = 'CVerror', ylim = c(0.4,1))
+print("Done")
