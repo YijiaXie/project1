548c4accbf66b96dd6ae41ac893ee5123df2105e
Feng <54845390+Acha0623@users.noreply.github.com>
2020-03-19 19:33:26 +0100

    Update do_PCA.r

--- do_PCA.r
+++ do_PCA.r
@@ -24,7 +24,7 @@ pca_all = read.table('./Gsample21.pca10.eigenvec')
 names(pca_all) = c('Population', 'Individual', 'PC1', 'PC2', 'PC3','PC4', 'PC5', 'PC6','PC7', 'PC8', 'PC9','PC10')
 pca_all$Population <- factor(pca_all$Population , levels =c("Gbb","Gbg","Ggd","Ggg"))
 pca_all = pca_all[order(pca_all$Population),]
-png("./PCA.All.png")
+png("./PCA.All.png",width=800,height=800)
 plot_pca_basic(pca_all, title = 'All gorilla',
 	xlab=paste0("PC1 (",first_pc("Gsample21.pca10.eigenval"),"% of variance)"),
 	ylab=paste0("PC2 (",second_pc("Gsample21.pca10.eigenval"),"% of variance)"))
@@ -35,7 +35,7 @@ pca_eastern = read.table('./esat.pca10.eigenvec')
 names(pca_eastern) = c('Population', 'Individual', 'PC1', 'PC2', 'PC3','PC4', 'PC5', 'PC6')
 pca_eastern$Population <- factor(pca_eastern$Population , levels =c("Gbb","Gbg"))
 pca_eastern = pca_eastern[order(pca_eastern$Population),]
-png("./Eastern.png")
+png("./Eastern.png",width=800,height=800)
 plot_pca_basic(pca_eastern, title = 'Eastern gorilla',
 	xlab=paste0("PC1 (",first_pc("esat.pca10.eigenval"),"% of variance)"),
 	ylab=paste0("PC2 (",second_pc("esat.pca10.eigenval"),"% of variance)"))
@@ -45,7 +45,7 @@ pca_western = read.table('west.pca10.eigenvec')
 names(pca_western) = c('Population', 'Individual', 'PC1', 'PC2', 'PC3','PC4')
 pca_western$Population <- factor(pca_western$Population , levels =c("Ggg","Ggd"))
 pca_western = pca_western[order(pca_western$Population),]
-png("./Western.png")
+png("./Western.png",width=800,height=800)
 plot_pca_basic(pca_western, title = 'Western gorilla',
 	xlab=paste0("PC1 (",first_pc("west.pca10.eigenval"),"% of variance)"),
 	ylab=paste0("PC2 (",second_pc("west.pca10.eigenval"),"% of variance)"))
