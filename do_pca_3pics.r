f871712632096e6348ab8413c4df257c983d5e70
Feng <54845390+Acha0623@users.noreply.github.com>
2020-03-21 20:35:02 +0100

    Create do_pca_3pics.r

---
+++ do_pca_3pics.r
@@ -0,0 +1,68 @@
+#PCA
+#存成三个图
+
+#calculate
+first_pc <- function(path){
+        egivals=read.table(path)
+        first=round((egivals[1,]/sum(egivals)*100),2)
+        return(first)
+}
+second_pc <- function(path){
+        egivals=read.table(path)
+        second=round((egivals[2,]/sum(egivals)*100),2)
+        return(second)
+}
+
+colors=c("darkred","chartreuse3","palevioletred1","dodgerblue2")
+#plot
+plot_pca_basic <- function(pca_df, title, xlab, ylab){
+    plot(pca_df$PC1 , pca_df$PC2, 
+         col = ifelse(pca_df$Population == 'Gbb', 'darkred', 
+                      ifelse(pca_df$Population == 'Gbg',"chartreuse3", 
+               ifelse(pca_df$Population == 'Ggd',"palevioletred1",'dodgerblue2'))),
+        pch = 16, main = title, xlab=xlab, ylab=ylab,cex.main=2, cex.lab=1.5, cex.axis=1.5, cex=2)
+    legend(x="topleft", legend = levels(pca_df$Population), cex=2,pch=16,col = colors)
+}
+plot_pca_basic1 <- function(pca_df, title, xlab, ylab){
+    plot(pca_df$PC1 , pca_df$PC2, col = pca_df$Population, pch = 16, main = title, xlab=xlab, ylab=ylab,
+         cex.main=2, cex.lab=1.5, cex.axis=1.5, cex=2)
+    legend(x="topleft", legend = levels(pca_df$Population), cex=2,
+           fill = palette()[1:length(levels(pca_df$Population))],)
+}
+
+#All
+colors=c("darkred","chartreuse3","palevioletred1","dodgerblue2")
+pca_all = read.table('./Gsample21.pca10.eigenvec')
+names(pca_all) = c('Population', 'Individual', 'PC1', 'PC2', 'PC3','PC4', 'PC5', 'PC6','PC7', 'PC8', 'PC9','PC10')
+pca_all$Population <- factor(pca_all$Population , levels =c("Gbb","Gbg","Ggd","Ggg"))
+pca_all = pca_all[order(pca_all$Population),]
+png("./PCA.All.png",res=50)
+plot_pca_basic(pca_all, title = 'All gorilla',
+        xlab=paste0("PC1 (",first_pc("Gsample21.pca10.eigenval"),"% of variance)"),
+        ylab=paste0("PC2 (",second_pc("Gsample21.pca10.eigenval"),"% of variance)"))
+
+
+#eastern
+colors=c("darkred","chartreuse3")
+pca_eastern = read.table('./east.pca10.eigenvec')
+names(pca_eastern) = c('Population', 'Individual', 'PC1', 'PC2', 'PC3','PC4', 'PC5', 'PC6')
+pca_eastern$Population <- factor(pca_eastern$Population , levels =c("Gbb","Gbg"))
+pca_eastern = pca_eastern[order(pca_eastern$Population),]
+png("./Eastern.png",res=50)
+plot_pca_basic(pca_eastern, title = 'Eastern gorilla',
+        xlab=paste0("PC1 (",first_pc("east.pca10.eigenval"),"% of variance)"),
+        ylab=paste0("PC2 (",second_pc("east.pca10.eigenval"),"% of variance)"))
+
+#western
+colors=c("dodgerblue2","palevioletred1")
+pca_western = read.table('west.pca10.eigenvec')
+names(pca_western) = c('Population', 'Individual', 'PC1', 'PC2', 'PC3','PC4')
+pca_western$Population <- factor(pca_western$Population , levels =c("Ggg","Ggd"))
+pca_western = pca_western[order(pca_western$Population),]
+png("./Western.png",res=50)
+plot_pca_basic(pca_western, title = 'Western gorilla',
+        xlab=paste0("PC1 (",first_pc("west.pca10.eigenval"),"% of variance)"),
+        ylab=paste0("PC2 (",second_pc("west.pca10.eigenval"),"% of variance)"))
+
+
+print("Done plotting PCAs")
