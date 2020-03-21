e58a28a3648bcf300af9285e9695931ec56c0b93
Feng <54845390+Acha0623@users.noreply.github.com>
2020-03-21 17:56:49 +0100

    Update do_Admixture.r

--- do_Admixture.r
+++ do_Admixture.r
@@ -3,8 +3,9 @@ snpk2=read.table("./Gsample21.clean.2.Q")
 snpk3=read.table("./Gsample21.clean.3.Q")
 snpk4=read.table("./Gsample21.clean.4.Q")
 snpk5=read.table("./Gsample21.clean.5.Q")
+snpk6=read.table("./Gsample21.clean.6.Q")
 names=c('Gbb-Maisha', 'Gbb-N010_Turimaso', 'Gbb-SC_EGWGS5386356_Zirikana',
-'Gbb-SC_EGWGS5389138_Imfura', 'Gbb-SC_EGWGS5389140_Tuck', 
+'Gbb-SC_EGWGS5389138_Imfura', 'Gbb-SC_EGWGS5389140_Tuck',
 'Gbb-SC_EGWGS5389618_Kaboko', 'Gbb-Umurimo', 'Gbg-9732_Mkubwa',
 'Gbg-A929_Kaisi', 'Gbg-A967_Victoria', 'Gbg-N011_Pinga', 'Gbg-N012_Dunia',
 'Gbg-SC_EGWGS5389141_Itebero', 'Gbg-SC_EGWGS5389142_Ntabwoba',
@@ -16,22 +17,25 @@ names=c('Gbb-Maisha', 'Gbb-N010_Turimaso', 'Gbb-SC_EGWGS5386356_Zirikana',
 'Ggg-B650_Katie', 'Ggg-KB3782_Vila', 'Ggg-KB3784_Dolly', 'Ggg-KB4986_Katie',
 'Ggg-KB5792_Carolyn', 'Ggg-KB5852_Helen', 'Ggg-KB6039_Oko', 'Ggg-KB7973_Porta',
 'Ggg-X00108_Abe', 'Ggg-X00109_Tzambo')
-png('./admixture.png', width=1200, height=800,res=100)
-par(mfrow=c(4,1))
+png('./admixture.png', width=1200, height=1100,res=100)
+par(mfrow=c(5,1), mar=c(8, 4.1, 4.1, 2.1))
 barplot(t(as.matrix(snpk2)),
         col= c("lightblue","Dark red"),
          border=NA, main="K=2",
-         cex.names=0.55, las=2, ylab="ancestry")
+         cex.names=0.7, las=2, ylab="ancestry")
 barplot(t(as.matrix(snpk3)),
         col= c("lightgreen","Dark red","lightblue"),
         border=NA, main="K=3",
-        cex.names=0.55, las=2, ylab="ancestry")
+        cex.names=0.7, las=2, ylab="ancestry")
 barplot(t(as.matrix(snpk4)),
         col= c("lightgreen","Dark red","lightblue","yellow"),
-        border=NA, main="K=4",cex.names=0.55, las=2,
+        border=NA, main="K=4", cex.names=0.7, las=2,
         ylab="ancestry")
 barplot(t(as.matrix(snpk5)),
         col= c("lightgreen","Dark red","lightblue","yellow","pink"),
-        border=NA, main="K=5", names.arg=(names), cex.names=0.55, las=2,
+        border=NA, main="K=5", cex.names=0.7, las=2,
         ylab="ancestry")
-print("Done admixture plotting")
+barplot(t(as.matrix(snpk6)),
+        col= c("lightgreen","Dark red","lightblue","yellow","pink", "purple"),
+        border=NA, main="K=6", names.arg=(names), cex.names=0.7, las=2,
+        ylab="ancestry")
