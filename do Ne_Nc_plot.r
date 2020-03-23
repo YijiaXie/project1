0fb12d2a52b996118cb0371b1c466b97e93642ba
Feng <54845390+Acha0623@users.noreply.github.com>
2020-03-23 17:21:32 +0100

    Update do Ne_Nc_plot.r

--- do Ne_Nc_plot.r
+++ do Ne_Nc_plot.r
@@ -1,11 +1,8 @@
-
 # Load in the Ne and Nc estimates
-
 Ne = read.table("./Ne_estimates.txt",sep = '\t', header = TRUE)
 Nc = read.table("./Nc_estimates.txt",sep = '\t', header = TRUE)
 
 ## Use the merge command to join them
-
 estimates = merge(Ne, Nc)
 estimates$ratio = estimates$Ne_est / estimates$Nc_est
 
@@ -21,15 +18,12 @@ for_ratio_barplot = data.matrix(t(estimates[,'ratio']))
 colnames(for_ratio_barplot) = estimates$Pop
 
 
-png('./Ne_estimates.png', width=1000,height =1000,res=100)
+png('./Ne_and_Nc.png', width=1000,height =1000,res=100)
 par(mfrow=c(2,2),mar=c(7.5,5.5,3,3))
-#par(mar=c(10,6,4,2))
 barplot(for_barplot['Ne_est',], col = "white", beside = TRUE, las=2,width=0.8,
        main = "Ne estimates for each population",ylab = '',cex.main=1.4)
 mtext('Ne', side=2, line=4.2)
 
-#png('./Ne-Nc_ratios.png',width=650,height =650,res=100)
-#par(mar=c(10,6,4,2))
 barplot(for_ratio_barplot, col = "gray", beside = TRUE, las=2, #axes = FALSE,
        main = "Ne/Nc ratios for each population",ylab = '',cex.main=1.4)
 mtext('Ne/Nc ratio', side=2, line=4.2)
@@ -45,8 +39,6 @@ legend("top",
 )
 
 # same plot with a log y axis
-#png('./Ne_and_Nc_estimates_log-scaled.png', width=650,height =650,res=100)
-#par(mar=c(10,6,4,2))
 barplot(for_barplot, col = c("white","grey12"), beside = TRUE, las=2,
         log = 'y',
        main = "Ne and Nc estimates for each population",ylab = '',cex.main=1.4)
@@ -55,6 +47,5 @@ legend("top",
   c("Ne_est","Nc_est"),
   fill =  c("white","grey12")
 )
-
 print("Done Ne_Nc_plot")
 
